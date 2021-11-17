40e113313b91e64ba668ad1127e3409eef6d43bf
Supperslash <32471649+Supperslash@users.noreply.github.com>
2021-11-16 22:27:15 -0600

    Push update.
    Final push to check the pins on the board. If you runit from the lowest to the highest. Change them to select the range.

---
+++ Main.cpp
@@ -0,0 +1,25 @@
+/*
+  Project:          Pin Sniffer
+  Description:      Looks to find the pin numbers on esp boards.
+  Creation date:    11/16/2021-10:16:23
+  Author:           Maderdash
+  License:          Beerware
+*/
+//Enter the lowest pin to scan in START_PIN.
+//Enter the last pin to scan in the END_PIN.
+#define START_PIN 0 
+#define END_PIN 15
+void setup() {
+  Serial.begin(9600);
+  for (int count = START_PIN; count < END_PIN; count++) {
+    pinMode(count, INPUT_PULLUP);
+  }
+
+}
+
+void loop() {
+  for (int count = START_PIN; count < END_PIN; count++) {
+    if (digitalRead(count) == LOW);
+    Serial.println("the pin that is grounded out is... " + String(count));
+  }
+}
